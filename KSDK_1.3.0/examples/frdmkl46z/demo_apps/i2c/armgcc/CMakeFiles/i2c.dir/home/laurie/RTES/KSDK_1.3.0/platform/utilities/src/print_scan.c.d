ccb07f9ffaf59b5d24640f0794a713d5a8e04a1e
Laurie Kirkcaldy - Toothless, Manjaro <lauriekirkcaldy@gmail.com>
2016-10-20 14:23:29 +0100

    Hmpf (mouse works)

--- KSDK_1.3.0/examples/frdmkl46z/demo_apps/i2c/armgcc/CMakeFiles/i2c.dir/home/laurie/RTES/KSDK_1.3.0/platform/utilities/src/print_scan.c.d
+++
@@ -1,5 +0,0 @@
-CMakeFiles/i2c.dir/home/laurie/RTES/KSDK_1.3.0/platform/utilities/src/print_scan.c.obj: \
- /home/laurie/RTES/KSDK_1.3.0/platform/utilities/src/print_scan.c \
- /home/laurie/RTES/KSDK_1.3.0/platform/utilities/src/print_scan.h
-
-/home/laurie/RTES/KSDK_1.3.0/platform/utilities/src/print_scan.h:
--- KSDK_1.3.0/examples/frdmkl46z/demo_apps/i2c/main.c
+++ KSDK_1.3.0/examples/frdmkl46z/demo_apps/i2c/main.c
@@ -4,6 +4,9 @@
 #include "accel_i2c.c"
 #include "touch.c"
 
+#define DEADZONE 20
+#define MAXCLICK 60
+
 int main() {
   hardware_init();
   dbg_uart_init();
@@ -13,31 +16,57 @@ int main() {
   //write_i2c(ACCEL_WRITE_CMD,ACCEL_CTRL_1,0b00000001); // activate the accel
   PRINTF("Initialised\r\n");
 
-
+  uint16_t first;
+  uint16_t second;
+  uint16_t touchCal = 0;
+  int difference;
+  bool lClick = 0;
+  uint8_t mClick = 0;
+  bool rClick = 0;
 
   while(1) {
     // Touch sensor calibration
-    uint16_t touchCal = 0;
-    uint16_t leftTouch, rightTouch;
     for(int i=0;i<100;i++) {
-      leftTouch = touch_read(9);
-      rightTouch = touch_read(10);
+     first = touch_read(9);
+     second = touch_read(10);
 
-      if(leftTouch>rightTouch+1 || rightTouch>leftTouch+1) {
-        PRINTF("ERROR: Finger on sensor during calibration (Levels %d %d)\r\n",leftTouch,rightTouch);
-        break;
-      } else {
-        touchCal+=leftTouch;
-      }
+     if(first>second+1 || second>first+1) {
+       //PRINTF("ERROR: Finger on sensor during calibration (Levels %d %d)\r\n",leftTouch,rightTouch);
+       break;
+     } else {
+       touchCal+=first;
+     }
     }
     touchCal = touchCal/100;
-    PRINTF("Calibration level: %d\r\n",touchCal);
-    OSA_TimeDelay(100);
-    uint16_t first = touch_read(9);
-    uint16_t second = touch_read(10);
 
-    PRINTF("Touch %d %d\r\n",first,second);
-    OSA_TimeDelay(1000);
+    //OSA_TimeDelay(100);
+    first = touch_read(9);
+    second = touch_read(10);
+    difference = first - second;
+
+    //PRINTF("%d %d %d\r\n",first,second,difference);
+    if(difference<-DEADZONE) {
+      //PRINTF("LMB\r\n");
+      lClick = true;
+    } else if(difference>DEADZONE) {
+      //PRINTF("RMB\r\n");
+      rClick = true;
+    } if(first>touchCal+1 || touchCal>first+1 || second>touchCal+1 || touchCal>second+1) {
+      if(abs(difference)<DEADZONE) {
+        //PRINTF("MMB\r\n");
+        if(mClick++>MAXCLICK) mClick=MAXCLICK;
+      } else {
+        if(mClick--<1) mClick=0;
+      }
+    } else {
+      lClick = false;
+      if(mClick--<1) mClick=0;
+      rClick = false;
+    }
+    if(lClick) PRINTF("LMB\r\n");
+    if(mClick==MAXCLICK) PRINTF("!!!!MMB\r\n");
+    if(rClick) PRINTF("RMB\r\n");
+    //OSA_TimeDelay(1000);
 
   }
 
